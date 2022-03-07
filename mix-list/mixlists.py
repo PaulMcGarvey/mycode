276009f00df7f65f77ed2851450df057d9c1f7d5
PMcG <PaulMcGarvey@users.noreply.github.com>
2022-03-07 20:59:46 +0000

    mixed lists

---
+++ mix-list/mixlists.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# define the  list
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+# print only the IP addresses
+print("The ip addresses are:", iplist[3], iplist[4], sep = "\n")
+
