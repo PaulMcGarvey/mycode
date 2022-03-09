92ffef6bb95807467131edafed6f79675d169bcf
PMcG <PaulMcGarvey@users.noreply.github.com>
2022-03-09 17:27:32 +0000

    Looping techniques

---
+++ fact/looping2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   For - Using a file's lines as a source for the for-loop"""
+
+# open file in read mode
+with open("dns_servers.txt", "r") as dnsfile:
+    
+
+    # create list of lines
+    dnslist = dnsfile.readlines()
+
+    # loop over lines
+    for svr in dnslist:
+        #print and end without a newline
+        print(svr, end="") # the line we read ALREADY contains a \n (newline)
+    
+
+
